03ae209ff7b67635df999514e9ff32371985735c
Z-i-n-k-s <zishancodes1@gmail.com>
2023-11-26 21:14:01 +0600

    2nd test

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,18 +15,61 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: AppColor.backGroundColor,
       body: SafeArea(
-        minimum: const EdgeInsets.symmetric(horizontal: 20).copyWith(top: 50),
-        child: Column(children: [
-          Row(
-            children: [
-              Image.asset(
-                AppAssets.menuIcon,
-                width: 40,
-                fit: BoxFit.contain,
-              ),
-            ],
-          )
-        ]),
+        minimum: const EdgeInsets.symmetric(horizontal: 20).copyWith(top: 45),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  AppAssets.menuIcon,
+                  width: 42,
+                  fit: BoxFit.contain,
+                ),
+                Container(
+                  height: 40,
+                  width: 44,
+                  //  margin: EdgeInsets.only(top: 40),
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: AppColor.buttonColor,
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: AppColor.whiteColor,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColor.shadowColor,
+                        spreadRadius: 2.2,
+                        blurRadius: 5,
+                        offset: Offset(-3, 1),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/res/colors/app_color.dart
+++ lib/res/colors/app_color.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static Color backGroundColor = const Color.fromARGB(255, 240, 240, 240);
+  static Color backGroundColor = const Color.fromARGB(255, 255, 253, 228);
 
   static Color heroColor = const Color.fromARGB(215, 152, 255, 152);
+
+  static Color buttonColor = const Color(0xFF3e4a3d);
+
+  static Color whiteColor = Colors.white;
+
+  static Color shadowColor = Color.fromARGB(255, 107, 116, 107);
 }
