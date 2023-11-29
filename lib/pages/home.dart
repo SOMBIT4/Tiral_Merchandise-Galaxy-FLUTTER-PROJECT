970bedcfe0e9a864789361a08e78c61d24d13222
Z-i-n-k-s <zishancodes1@gmail.com>
2023-11-29 20:03:39 +0600

    added product

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_3/buttons/button.dart';
 import 'package:flutter_application_3/res/app_assets/app_assets.dart';
 import 'package:flutter_application_3/res/colors/app_color.dart';
 
@@ -17,6 +18,7 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         minimum: const EdgeInsets.symmetric(horizontal: 20).copyWith(top: 45),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(
               height: 5,
@@ -93,6 +95,103 @@ class _HomePageState extends State<HomePage> {
                   size: 42,
                 ),
               ],
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            Text(
+              'Product',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: const EdgeInsets.all(14),
+              width: 150,
+              decoration: BoxDecoration(
+                color: AppColor.whiteColor,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: AspectRatio(
+                      aspectRatio: 2 / 1.75,
+                      child: Image.asset(
+                        AppAssets.foodBurger,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Name',
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 0,
+                  ),
+                  Text(
+                    'description',
+                    style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                          fontWeight: FontWeight.w300,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Price : ',
+                        style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                              fontWeight: FontWeight.w500,
+                            ),
+                      ),
+                      const SizedBox(
+                        width: 2,
+                      ),
+                      Text(
+                        '\$280',
+                        style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                              fontWeight: FontWeight.w500,
+                            ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MyButton(
+                          onTap: () {},
+                          child: Text(
+                            "Add To Cart",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(
+                                  color: AppColor.whiteColor,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                          )),
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
